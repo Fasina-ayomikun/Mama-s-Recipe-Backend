05de424feb6b43bde5741dc895b180669d3bacd1
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-25 22:26:12 +0100

    Update addCookies.js

--- utils/addCookies.js
+++ utils/addCookies.js
@@ -5,7 +5,7 @@ const addCookies = ({ res, user }) => {
   const token = createToken(user);
   const maxAge = 60 * 60 * 1000 * 24;
   res.cookie("token", token, {
-    httpOnly:process.env.NODE_ENV === "production",
+    httpOnly:false,
     secure: process.env.NODE_ENV === "production",
     expires: new Date(Date.now() +  maxAge),
     signed: true,

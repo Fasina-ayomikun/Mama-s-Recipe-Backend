033560b700999ca19969bf486eb2aa55295eb341
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-25 20:14:37 +0100

    Update addCookies.js

--- utils/addCookies.js
+++ utils/addCookies.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const createToken = require("./createToken");
 
 const addCookies = ({ res, user }) => {
   const token = createToken(user);
   const maxAge = 60 * 60 * 1000 * 24;
   res.cookie("token", token, {
-    httpOnly:process.env.NODE_ENV === "production",
+    httpOnly:true,
     secure: process.env.NODE_ENV === "production",
     expires: new Date(Date.now() +  maxAge),
     signed: true,

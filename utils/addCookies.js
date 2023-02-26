46dffada9d1ded7f118032fa9d65594318800010
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-26 08:24:10 +0100

    Update addCookies.js

--- utils/addCookies.js
+++ utils/addCookies.js
@@ -5,12 +5,12 @@ const addCookies = ({ res, user }) => {
   const token = createToken(user);
   const maxAge = 60 * 60 * 1000 * 24;
   res.cookie("token", token, {
-    httpOnly:false,
+    httpOnly:true,
     secure: process.env.NODE_ENV === "production",
     expires: new Date(Date.now() +  maxAge),
     signed: true,
     domain: 'https://mama-s-recipe.vercel.app',
-    sameSite:false,
+    sameSite:'none',
   });
 };
 module.exports = {

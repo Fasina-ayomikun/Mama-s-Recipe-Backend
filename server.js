4ecd04d6c7ae1ae4c2b3003feeaf4b58d22d2397
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-27 20:26:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(fileUploader({ useTempFiles: true }));
-app.use("/assets", express.static("assets"));
-
 app.use(
   cors({
     origin: process.env.FRONTEND_LINK,
@@ -25,9 +23,6 @@ cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
-});
-app.post("/", (req, res) => {
-  res.json({ msg: "msg" });
 });
 
 app.use("/auth", require("./routes/auth"));

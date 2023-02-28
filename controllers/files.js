ff4a68806986e5c3d46728808330c9ff8c38849f
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-28 10:32:30 +0100

    Update files.js

--- controllers/files.js
+++ controllers/files.js
@@ -21,11 +21,17 @@ const uploadImage = async (req, res) => {
     }
     const result = await cloudinary.uploader.upload(
       req.files.image.tempFilePath,
-      {
-        use_filename: true,
-        folder: "Mama's-Recipe",
-         secure: true ,
-      }
+       {
+    use_filename: true,
+    folder: "Mama's-Recipe",
+    secure: true,
+    overwrite: true,
+    // use /tmp directory for temporary file storage
+    // so that it can be written to
+    use_temp_file: true,
+    unique_filename: false,
+    temp_file_prefix: '/tmp/'
+  }
     );
 // //     console.log(result);
 // if (fs.existsSync(req.files.image.tempFilePath)) {

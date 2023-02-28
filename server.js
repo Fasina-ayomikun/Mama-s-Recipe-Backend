5dc6e7986954b4510afb3c4bfbeea36242601d6b
Deeyah <101244317+Fasina-ayomikun@users.noreply.github.com>
2023-02-28 09:03:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(
     credentials: true,
   })
 );
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUD_NAME,
+//   api_key: process.env.CLOUD_API_KEY,
+//   api_secret: process.env.CLOUD_API_SECRET,
+// });
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // replace "*" with the domain of your React application

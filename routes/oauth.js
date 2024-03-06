b776c03a7e3c424edadda3b2731f6c812feda6ab
Fasina Ayomikun <ayomikunfasina240@gmail.com>
2024-03-06 09:04:58 +0100

    minor fixes

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("../passport/passport");
-const cors = require("cors");
 const { addCookies } = require("../utils/addCookies");
 
 router.get(
